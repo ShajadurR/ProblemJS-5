7f89e9138125a7a8e31f42f42ad48d1b772d90ad
ShajadurR <shajadurrrahman@gmail.com>
2024-10-18 18:37:44 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Problem-5
+
+// Free Drinks
+// - Burger more than 500k: free Coke
+// - Else Coke: 30tk
+
+
+const haveMoney = 400;
+
+if(haveMoney >= 500){
+    console.log('burger er sathe free coke den')
+}
+else{
+    console.log('taka poysa nai sudhu coke kheye gola vijai')
+};
